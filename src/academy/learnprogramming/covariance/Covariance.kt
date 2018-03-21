4bbc6ab4fdc225b909eb9aeafd69ca7daad01ecc
Robert Lowe <robertmlowe@rmlowe.com>
2018-03-21 13:49:18 +0800

    Generics: Covariance

--- src/academy/learnprogramming/covariance/Covariance.kt
+++ src/academy/learnprogramming/covariance/Covariance.kt
@@ -3,15 +3,15 @@ package academy.learnprogramming.covariance
 
 fun main(args: Array<String>) {
 
-    val shortList: MutableList<Short> = mutableListOf(1, 2, 3, 4, 5)
-    //convertToInt(shortList)
-
+    val shortList: List<Short> = listOf(1, 2, 3, 4, 5)
+    convertToInt(shortList)
 }
 
-fun convertToInt(collection: MutableList<Number>) {
+fun convertToInt(collection: List<Number>) {
     for (num in collection) {
         println("${num.toInt()}")
     }
+    //collection.add(25.3)
 }
 
 fun tendGarden(roseGarden: Garden<Rose>) {
@@ -30,6 +30,13 @@ class Rose: Flower() {
 
 }
 
-class Garden<out T: Flower> {
+class Garden<out T: Flower>(private var something: T) {
+
+    val flowers: List<T> = listOf()
+
+    fun pickFlower(i: Int): T = flowers[i]
+    fun plantFlower(flower: T) {
+
+    }
 
 }
